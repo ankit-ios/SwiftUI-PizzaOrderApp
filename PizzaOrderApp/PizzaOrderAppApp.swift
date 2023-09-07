ff0b69cef1d222cf78968308ea89d60fa651393b
Ankit Sharma <ankit.sharma10@publicissapient.com>
2023-09-07 11:07:59 +0530

    Initial Commit

---
+++ PizzaOrderApp/PizzaOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaOrderAppApp.swift
+//  PizzaOrderApp
+//
+//  Created by Ankit Sharma on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PizzaOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
